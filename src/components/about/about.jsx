c2a89c15dd9c9d5767488e98f3e49d401d4a08f1
AlkMiist <michael.lawson@epitech.eu>
2020-11-10 11:48:28 +0100

    contact and about page added

---
+++ src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './about.css';
+
+export default class about extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
